6485abeb419337c1b24c9b0f45407a8eec83de30
duc <hoangtongvietduc@gmail.com>
2026-07-09 22:07:11 +0700

    fix(frontend): fix Image name conflict with lucide icon by using document.createElement

--- frontend-react/src/pages/Home.tsx
+++ frontend-react/src/pages/Home.tsx
@@ -93,7 +93,7 @@ const Home: React.FC = () => {
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = (event) => {
-        const img = new Image();
+        const img = document.createElement('img');
         img.src = event.target?.result as string;
         img.onload = () => {
           const canvas = document.createElement('canvas');
